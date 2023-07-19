7c3025d6ff981198dd0f2b4a4aa3837c00a8b42f
hakan dereli <hakandereli28@gmail.com>
2023-07-19 20:56:21 +0300

    eclipse shorcut added

--- src/main/java/com/hakandereli/ide_shortcuts/Intellij.java
+++ src/main/java/com/hakandereli/ide_shortcuts/Intellij.java
@@ -7,6 +7,19 @@ package com.hakandereli.ide_shortcuts;
 public class Intellij {
 
     /**
-     * 1- Döngü ye sokacağın bir yapı varsa iter yazıp tab yaptığın da döngüyü otomatik senin için yazar 1-iter.png yi incele
+     * Bu kısayollar aslında ECLİPSE KISAYOLLARI DIR.
+     *
+     * ALT+INSERT       : generate getter setter implements equals hashcode tostring vs...
+     * ALT+V+P          : method parametrelerinin tiplerini gösterir.
+     * CTRL+D           : imleçin olduğu satırı siler.
+     * ALT+SHIFT+R      : method, class, interface, değişken vs için kullanıldığı tüm yerlerde rename yapar.
+     * (ALT+F1)+1       : Bir dosyanın bulunduğu konumu bulur.
+     *
+     * Karmaşık Olanlar:
+     *
+     * Döngü ye sokacağın bir yapı varsa iter yazıp tab yaptığın da döngüyü otomatik senin için yazar 1-iter.png yi incele
+     *
+     * Constructor da bir parametre tanımladın ve bunun construct üzerinde tanımlanması constuructor içinde de
+     *      * this.urun = urun gibi yazılması gerekiyor. parametreye gelip alt + enter yaparsan genereate field diye oluşturur.
      * */
 }
